f30a364a92269a3afc8c2b4039193a0e2d5e2831
Sarah Binney <sarah.binney@softwire.com>
2023-01-07 10:57:25 +0000

    working intersection demo

--- Test.gd
+++ Test.gd
@@ -1,11 +1,6 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,26 +8,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-#	if Input.is_action_pressed("ui_right"):
-#		$RigidBodyCircle.add_force(Vector2(0,0), Vector2(1,0))
-#	if Input.is_action_pressed("ui_left"):
-#		$RigidBodyCircle.add_force(Vector2(0,0), Vector2(-1,0))
-	if Input.is_action_just_pressed("ui_right"):
-		var areaPolygon = PoolVector2Array()
-		print($Area2D.position)
-		for point in $Area2D/CollisionPolygon2D.polygon:
-			areaPolygon.append(point + $Area2D.position)
-		print(areaPolygon)
-		var kinematicPolygon = PoolVector2Array()
-		for point in $KinematicBody2D/Polygon2D.polygon:
-			kinematicPolygon.append(point + $KinematicBody2D.position)
-		var intersection = Geometry.intersect_polygons_2d(
-			areaPolygon,# - $Area2D.position,
-			kinematicPolygon)# - $KinematicBody2D.position)
-		#$Line2D.points = [Vector2(0, 100), Vector2(100, 0)]
-		$Line2D.points = intersection[0]
-		$IntersectionMarker.polygon = intersection[0]#.invert()
-		print(intersection)
+#	if Input.is_action_just_pressed("ui_right"):
+#		var areaPolygon = PoolVector2Array()
+#		print($Area2D.position)
+#		for point in $Area2D/CollisionPolygon2D.polygon:
+#			areaPolygon.append(point + $Area2D.position)
+#		print(areaPolygon)
+#		var kinematicPolygon = PoolVector2Array()
+#		for point in $KinematicBody2D/Polygon2D.polygon:
+#			kinematicPolygon.append(point + $KinematicBody2D.position)
+#		var intersection = Geometry.intersect_polygons_2d(
+#			areaPolygon,# - $Area2D.position,
+#			kinematicPolygon)# - $KinematicBody2D.position)
+#		#$Line2D.points = [Vector2(0, 100), Vector2(100, 0)]
+#		$Line2D.points = intersection[0]
+#		$IntersectionMarker.polygon = intersection[0]#.invert()
+#		print(intersection)
 	pass
 
 func _physics_process(delta):
@@ -49,9 +40,23 @@ func _physics_process(delta):
 #	$IntersectionMarker.position += Vector2(vh, vv) * 10
 #	print($IntersectionMarker.position)
 	var collision_info = $KinematicBody2D.move_and_collide(Vector2(vh, vv) * 100 * delta)
-	#var intersection = Geometry.intersect_polygons_2d($Area2D/CollisionPolygon2D.polygon, $KinematicBody2D/Polygon2D.polygon)
-	#print(intersection)
-	#$IntersectionMarker.polygon = intersection
+	mark_intersections($KinematicBody2D, $Area2D)
+
+func mark_intersections(_body1, _body2): # TODO actually use these
+	var polygon1 = PoolVector2Array()
+	print($Area2D.position)
+	for point in $Area2D/CollisionPolygon2D.polygon:
+		polygon1.append(point + $Area2D.position)
+	print(polygon1)
+
+	var polygon2 = PoolVector2Array()
+	for point in $KinematicBody2D/Polygon2D.polygon:
+		polygon2.append(point + $KinematicBody2D.position)
+
+	var intersections = Geometry.intersect_polygons_2d(polygon1, polygon2)
+	if intersections.size() > 0:
+		$IntersectionMarker.polygon = intersections[0]
+		# TODO cover higher order intersections / concave shapes
 
 
 func _on_Area2D_body_entered(body):
